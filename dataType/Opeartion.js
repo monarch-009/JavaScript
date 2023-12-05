61f5bd43276efac2141a5c016832338e63d2c332
monarch-009 <adityasinghmoni@gmail.com>
2023-12-05 11:12:03 +0530

    data type in js

---
+++ dataType/Opeartion.js
@@ -0,0 +1,23 @@
+// ********************        Operation        ********************//
+
+let n1 = 10;
+let n2 = 20;
+
+console.log(n1 + n2); // 30
+console.log(n1 - n2); // -10
+console.log(n1 * n2); // 200
+console.log(n1 / n2); // 0.5
+console.log(n1 % n2); // 10
+console.log(n1 ** n2); // 100000000000000000000
+
+let s1 = "Hello ";
+let s2 = "Aditya Singh ";
+let s3 = s1 + s2;
+
+console.log(s1 + s2); // Hello Aditya Singh
+console.log(s3); // Hello Aditya Singh
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123 
+console.log(1 + 2 + "3"); // 33
