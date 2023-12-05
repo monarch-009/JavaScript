3d9b07922d310c639e3466bca7821b1681f907b1
monarch-009 <adityasinghmoni@gmail.com>
2023-12-05 08:42:41 +0530

    dataType in js

---
+++ dataType/index.js
@@ -0,0 +1,22 @@
+"use strict"; //use strict mode treat al js as newer version
+
+// alert("Hello World"); // we can use alert in browser
+
+//Primitive Data Types
+let name = "Aditya"; //string => "",'',``
+let age = 30; //number => int,float => 2 to power 53
+let isApproved = true; //boolean => true,false
+let firstName = undefined; //undefined => default value of a variable
+let lastName = null; //null => explicitly cleared by the developer
+//bigInt => 2 to power 53
+//symbol => unique identifier
+
+console.log(typeof null); //object -> bug in js
+
+//Reference Data Types
+//Object
+//Array
+//Function
+//Date
+//RegExp
+//Error 
