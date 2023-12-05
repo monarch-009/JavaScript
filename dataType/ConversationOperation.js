1be3adad19b80a176f7207fc88a957d3a366a895
monarch-009 <adityasinghmoni@gmail.com>
2023-12-05 11:01:09 +0530

    data type in js

---
+++ dataType/ConversationOperation.js
@@ -0,0 +1,14 @@
+let score = "45";
+let score1 = "Abc"
+
+console.log(typeof score); // string
+console.log(typeof score1); // string
+
+let valueInNumber1 = Number(score);
+let valueInNumber2 = Number(score1);
+
+console.log(typeof valueInNumber); // number
+console.log(typeof valueInNumber2); // number
+
+console.log(valueInNumber1); // 45
+console.log(valueInNumber2); // NaN
