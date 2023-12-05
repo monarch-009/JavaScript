742fd95cc86eaa976801870cdc1a680ddc13d32f
monarch-009 <adityasinghmoni@gmail.com>
2023-12-05 15:15:55 +0530

    memory

---
+++ Memory/memory.js
@@ -0,0 +1,21 @@
+// Primitive -> stored in stack
+// Reference -> stored in heap
+
+let name1 = "Aditya Singh";
+
+let name2 = name1; //copy by value
+name2 = "Rahul Singh"; //copy by value  
+
+console.log(name1);
+console.log(name2);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne; //copy by reference
+userTwo.email = "adi@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
