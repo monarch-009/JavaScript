53a1dbc85fe3ff994bd7b4d9811eea5c125f8182
monarch-009 <adityasinghmoni@gmail.com>
2023-12-16 10:16:55 +0530

    maths

---
+++ dataType/maths_num.js
@@ -0,0 +1,22 @@
+const score = 100; // number type data 
+// console.log(score);
+
+const balance = new Number(1000); // number type data 
+// console.log(balance);
+
+// console.log(balance.toString().length); // toString() is a method of Number class
+
+// console.log(balance.toFixed(2)); // toFixed() is a method of Number class
+
+// console.log(typeof score);
+
+const otherNum = 159.685 // number type data
+// console.log(otherNum.toPrecision(2)); // toPrecision() is a method of Number class
+
+const num = 100000; // number type data
+// console.log(num.toLocaleString('en-IN')); // toExponential() is a method of Number class
+
+
+
+
+
