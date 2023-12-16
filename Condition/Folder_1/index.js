850ed43a944f373867b3a71a5e36b2906dbdd55f
monarch-009 <adityasinghmoni@gmail.com>
2023-12-16 16:02:32 +0530

    if else in js

---
+++ Condition/Folder_1/index.js
@@ -0,0 +1,27 @@
+// Test: Condition/Folder_1/index.js
+true;
+false;
+
+console.log(3 > 5 - 5);
+
+console.log(5=== '5.00');
+
+if (false) {
+    console.log('Hello World');
+}
+else{
+    console.log('Radhe Radhe');
+}
+
+let age = 14;
+if (age >= 18){
+    console.log('You are eligible for voting');
+}
+else if( age >= 17){
+    console.log('You are eligible for voting but you are not an adult');
+
+}
+else{
+    console.log('You are not eligible for voting');
+}
+
